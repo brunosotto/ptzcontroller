97dd70344f6d83f1ba8b56a4b3248fe751fddbdd
André Rds <andrerds.souza@gmail.com>
2020-08-03 15:38:34 -0300

    Add os ajustes tela ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Platform, MenuController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -48,8 +48,7 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private menuCtrl: MenuController
+    private statusBar: StatusBar, 
   ) {
     this.initializeApp();
   }
