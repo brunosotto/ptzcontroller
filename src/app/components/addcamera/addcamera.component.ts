509b2b16f1065c10f57e577620e3604c749a3cbd
André Rds <andrerds.souza@gmail.com>
2020-08-07 16:05:16 -0300

    update camera

--- src/app/components/addcamera/addcamera.component.ts
+++ src/app/components/addcamera/addcamera.component.ts
@@ -1,3 +1,4 @@
+import { OverlayService } from './../../services/overlay.service';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CamerasService } from 'src/app/services/cameras.service';
@@ -15,25 +16,42 @@ import { startWith } from 'rxjs/operators';
 })
 export class AddcameraComponent implements OnInit {
 
-private reload$ = new Subject();
+  private reload$ = new Subject();
 
-public form: FormGroup;
+  public form: FormGroup;
 
- public cams: ICamera[];
+  public cams: ICamera[];
 
+  configs = {
+    createNew: true,
+    action: 'Salvar',
+    actionChange: 'Salvar edição'
+  };
+
+  public editIndex = null;
+  public clicked = false;
+  id = new Date().getTime();
   constructor(
     private modalCtrl: ModalController,
     private camerasService: CamerasService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private overlayService: OverlayService) { }
 
   ngOnInit() {
+    if (!this.id) {
+      return;
+    }
     this.initForm();
+
     this.camerasService.getAllCamera().then((res: ICamera[]) => this.cams = res);
+
   }
 
   initForm(): void {
-    const id =  Date.now();
+
     this.form = this.fb.group({
+      index: [{ value: null, disabled: true }],
+      id: this.id,
       name: ['', Validators.required],
       ipaddress: ['', Validators.required],
       user: ['', Validators.required],
@@ -42,29 +60,62 @@ public form: FormGroup;
     });
   }
 
-  add(): void{
-    try {
-      console.log(this.form.value);
+  onAddSubmit(form): void {
+    const index = form.getRawValue().index;
+    if (index !== null) {
+      this.cams[index] = form.value;
+      const dataUpdate = this.cams[index];
+
+      this.reload$.pipe(startWith([null])).subscribe(_ => {
+        this.camerasService.updateCamera(dataUpdate).then(() => {
+        this.overlayService.toast({message: `${dataUpdate.name}  Atualizado com sucesso! `});
+      });
+      });
+    } else {
       const data = this.form.value;
       this.camerasService.addCamera(data);
-      this.form.reset();
-
-    } catch (error) {
-      console.log('ERrroroororor' , error);
+      console.log('Adicionando com sucesso  ', data);
     }
-   }
+    this.form.reset();
 
-    deleteCam(ipaddress) {
-    this.reload$.pipe(startWith([null])).subscribe(_ => {
-      this.camerasService.deleteCamera(ipaddress);
-      this.camerasService.getAllCamera().then((res: any) => {
-        return this.cams = res;
-      });
+  }
+  onEditSubmit(cam: ICamera, i) {
+    this.form.setValue({
+      index: i,
+      ...cam
     });
   }
+ deleteCam(ipaddress) {
+   this.overlayService.alert({
+     header: 'PTZ Controle',
+     message: `Deseja deletar Câmera ${ ipaddress } ?`,
+     buttons: [{
+       text: 'Não',
+       role: 'cancel',
+       handler: () => { console.log( 'Cancel');
+       }
+     }, {
+       text: 'Sim',
+       role: 'confirm',
+       handler: () => {
+              this.reload$.pipe(startWith([null])).subscribe(_ => {
+                    this.camerasService.deleteCamera(ipaddress);
+                    this.camerasService.getAllCamera().then((res: any) => {
+                      return this.cams = res;
+                    });
+                  });
+              this.overlayService.toast({message: 'Deletado com sucesso!'});
+              this.reload$.next();
+       }
+     }
+     ]
+   });
+  }
 
-    closeModal(cam: ICamera): void {
-        this.modalCtrl.dismiss(cam);
+
+
+  closeModal(cam: ICamera): void {
+    this.modalCtrl.dismiss(cam);
   }
 
 }
--- src/app/services/cameras.service.ts
+++ src/app/services/cameras.service.ts
@@ -3,6 +3,8 @@ import { Storage } from '@ionic/storage';
 import { ICamera } from './../models/camera.model';
 import { OverlayService } from './overlay.service';
 
+const CAMERA_STORAGE = 'cameras';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,57 +15,76 @@ export class CamerasService {
 
   constructor(private storage: Storage, private overlayService: OverlayService) { }
 
-   async addCamera(camera: ICamera)  {
-      let exist  = false;
-      const load = await this.overlayService.loading({message: 'Aguarde...'});
-      for ( const cam of this.cameras ){
-        if ( cam.ipaddress === camera.ipaddress ) {
-          exist = true;
-        }
+  async addCamera(camera: ICamera) {
+    let exist = false;
+    const load = await this.overlayService.loading({ message: 'Aguarde...' });
+    for (const cam of this.cameras) {
+      if (cam.ipaddress === camera.ipaddress) {
+        exist = true;
       }
-
-      if ( exist ) {
-        this.cameras = this.cameras.filter( cam => cam.ipaddress !== camera.ipaddress);
-      } else {
-        this.cameras.push( camera );
-      }
-      load.dismiss();
-
-      this.storage.set('cameras', this.cameras);
-
-
-      this.overlayService.toast({ message: 'Adicionado com sucesso.'});
-      return !exist;
-
-   }
-
-    async getAllCamera()  {
-     return this.storage.ready().then(async () => {
-      const cams = await this.storage.get('cameras');
-      this.cameras = cams || [];
-      return this.cameras;
-        }).catch((e) => console.log(e));
     }
 
+    if (exist) {
+      this.cameras = this.cameras.filter(cam => cam.ipaddress !== camera.ipaddress);
+    } else {
+      this.cameras.push(camera);
+    }
+    load.dismiss();
+
+    this.storage.set(CAMERA_STORAGE, this.cameras);
 
 
-   async getCamera( ipaddress ) {
-     await this.getAllCamera();
-     const cam = this.cameras.find( camera => camera.ipaddress === ipaddress );
-     return  cam || [];
+    this.overlayService.toast({ message: 'Adicionado com sucesso.' });
+    return !exist;
+
+  }
+
+  async getAllCamera() {
+    return this.storage.ready().then(async () => {
+      const cams = await this.storage.get(CAMERA_STORAGE);
+      this.cameras = cams || [];
+      return this.cameras;
+    }).catch((e) => console.log(e));
+  }
+
+
+
+  async getCamera(ipaddress) {
+    await this.getAllCamera();
+    const cam = this.cameras.find(camera => camera.ipaddress === ipaddress);
+    return cam || [];
     //  return ( cam ) ? this.cameras : false;
-   }
+  }
 
 
-   async updateCamera(cam: ICamera){
-     await this.getAllCamera();
-     const ArrayCamUpdate = this.cameras.find(camera => camera.ipaddress === cam.ipaddress );
-     this.storage.set('cameras', ArrayCamUpdate);
-   }
+  updateCamera(cam: ICamera): Promise<ICamera> {
 
-   deleteCamera(ipaddress) {
-     const ArrayCam = this.cameras.filter(res =>  res.ipaddress !== ipaddress  );
-     this.storage.set('cameras', ArrayCam);
-   }
+    return this.storage.get(CAMERA_STORAGE).then((res: ICamera[]) => {
+
+      if (!res || res.length === 0) { return null; }
+      // tslint:disable-next-line: prefer-const
+      let newCams: ICamera[] = [];
+
+      // tslint:disable-next-line: prefer-const
+      for (let i of res) {
+        if (i.id === cam.id) {
+
+          newCams.push(cam);
+        } else {
+          newCams.push(i);
+        }
+      }
+      return this.storage.set(CAMERA_STORAGE, newCams);
+    });
+
+
+  }
+
+
+
+  deleteCamera(ipaddress) {
+    const ArrayCam = this.cameras.filter(res => res.ipaddress !== ipaddress);
+    this.storage.set(CAMERA_STORAGE, ArrayCam);
+  }
 
 }
